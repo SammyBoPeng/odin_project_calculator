b692d440d45440666eb744553af48998064c9169
Sammy Peng <sammybopeng@gmail.com>
2025-10-25 22:10:00 +0800

    feat: add global variables for calculation
    - add num1, num2, and operator
    
    feat: add calculator operation function with error handling
    
    -- Implement operation() function with switch-based arithmetic operations
    - Include error handling for undefined inputs and division by zero

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let Operator;
+
 function add (a, b)
 {
     return a + b;
@@ -16,4 +20,35 @@ function multiply(a, b)
 function divide(a, b)
 {
     return a / b;
+}
+
+function opertation(num1, num2, operator){
+    // make sure every variable is defined
+    if (num1 === undefined){
+        console.log("ERROR: Number 1 is undefined");
+    }
+    if (num2 === undefined){
+        console.log("ERROR: Number 2 is undefined");
+    }
+    if (operator === undefined){
+        console.log("ERROR: Operator is undefined");
+    }
+
+    switch(operator){
+        case 'add':
+            return num1 + num2;
+
+        case 'subtract':
+            return num1 - num2;
+
+        case 'multiply':
+            return num1 * num2;
+
+        case 'divide':
+            if (num2 === 0){
+                console.log("ERROR: Zero divide error");
+                break;
+            }
+            return num1 / num2;
+    }
 }
